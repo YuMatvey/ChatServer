c846656f874f8bacbaea96c4328b18c11daec3fb
YuMatvey <fastymyt@gmail.com>
2024-07-06 15:54:38 +0300

    Chat for skillfactory

---
+++ src/Client.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+class Client implements Runnable {
+    Socket socket;
+    Scanner in;
+    PrintStream out;
+    ChatServer server;
+
+    public Client(Socket socket, ChatServer server) {
+        this.socket = socket;
+        this.server = server;
+        // запускаем поток
+        new Thread(this).start();
+    }
+
+    void recieve(String message) {
+        out.println(message);
+    }
+
+    public void run() {
+        try {
+            // получаем потоки ввода и вывода
+            InputStream is = socket.getInputStream();
+            OutputStream os = socket.getOutputStream();
+
+            // создаем удобные средства ввода и вывода
+            in = new Scanner(is);
+            out = new PrintStream(os);
+
+            // читаем из сети и пишем в сеть
+            out.println("Welcome to Chat!");
+            String input = in.nextLine();
+            while (!input.equals("bye")) {
+                server.sendAll(input);
+                input = in.nextLine();
+            }
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
